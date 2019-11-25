1c013de673c720d7bde71742f8e8aae8c28dd29e
Robert Reiss <reissaudiodesign@yahoo.com>
2019-11-24 22:31:41 -0700

    placed the lightsaber blade and hilt in Search page

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -16,7 +16,10 @@ export default class Search extends Component {
     render() {
         return (
             <div className="search-background">
+                <div className="lightsaber-hilt"></div>
+                <div className="lightsaber-blade"></div>
                 <div className="R2"></div>
+                <div className="R2-beam"></div>
             </div>
         )
     }
