71c986fadc1c40e581200ffbf2f763ca47f3c38a
Robert Reiss <reissaudiodesign@yahoo.com>
2019-11-25 12:31:23 -0700

    added more books with info to database

--- src/Components/Archives/Archives.js
+++ src/Components/Archives/Archives.js
@@ -10,7 +10,9 @@ export default class Archives extends Component {
         this.state = {
             archives: [],
             modalActivate: false,
-            book: ''
+            book: '',
+            smallNum: 0,
+            bigNum: 10
             // title: '',
             // author: '',
             // pages: '',
@@ -46,12 +48,26 @@ export default class Archives extends Component {
         })
     }
 
+    increment = () => {
+        this.setState({
+            bigNum: this.state.bigNum + 10,
+            smallNum: this.state.smallNum + 10
+        })
+    }
+
+    decrement = () => {
+        this.setState({
+            bigNum: this.state.bigNum - 10,
+            smallNum: this.state.smallNum - 10
+        })
+    }
 
     render() {
+        let filteredArchives = this.state.archives.filter((book, i) => i < this.state.bigNum && i >= this.state.smallNum)
         return (
             <div className="archives-background">
                 <div id="books-display">
-                    {this.state.archives.map((book, i) => {
+                    {filteredArchives.map((book, i) => {
                         return (
                             <Book
                                 onClick={this.modalFn}
@@ -65,31 +81,33 @@ export default class Archives extends Component {
                 {this.state.modalActivate &&
                     <div>
                         <button id="myBtn"></button>
-                        <div id="myModal" class="modal">
-                            <div class="modal-content">
-                                <span class="close">&times;</span>
+                        <div className="modal">
+                            <div className="modal-content">
+                                <span className="close">&times;</span>
                                 <p></p>
                             </div>
-                        </div>
-                        {/* Modal Content */}
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <span class="close">&times;</span>
-                                <h2>{this.state.book.title}</h2>
-                            </div>
-                            <div class="modal-body">
-                                <p>{this.state.book.author}</p>
-                                <p>{this.state.book.pages}</p>
-                                <p>{this.state.book.characters}</p>
-                                <p>{this.state.book.summary}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <h3>{this.state.book.image}</h3>
+                            {/* Modal Content */}
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <span className="close">&times;</span>
+                                    <h2>{this.state.book.title}</h2>
+                                </div>
+                                <div className="modal-body">
+                                    <p>{this.state.book.author}</p>
+                                    <p>{this.state.book.pages}</p>
+                                    <p>{this.state.book.characters}</p>
+                                    <p>{this.state.book.summary}</p>
+                                </div>
+                                <div className="modal-footer">
+                                    <h3>{this.state.book.image}</h3>
+                                </div>
                             </div>
                         </div>
                         {/* <div class="overlay"></div> */}
                     </div>
                 }
+                <button onClick={() => this.decrement()} className="prev">PREV</button>
+                <button onClick={() => this.increment()} className="next">NEXT</button>
             </div>
         )
     }
