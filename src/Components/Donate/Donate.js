3cd803df20a7b7ed843d8f79583d2e498c3bc74f
Robert Reiss <reissaudiodesign@yahoo.com>
2019-12-08 00:48:17 -0700

    added blue line under Watto's quote

--- src/Components/Donate/Donate.js
+++ src/Components/Donate/Donate.js
@@ -10,7 +10,9 @@ export default class Donate extends Component {
             <div className="donate-background">
                 <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css?family=Russo+One&display=swap" rel="stylesheet"></link>
-                <h1>"Republic credits are no good out here, I need something more real."</h1>
+                <h1>"Republic credits are no good out here, I need something more real."
+                <div id="watto-line"></div>
+                </h1>
                 <div className="donation-display">
                     <h2>Donate to the Holocron!</h2>
                     <StripeProvider apiKey="pk_test_ClZZ4tEa3nSFbIkdNeNdpRDI00l3NvF6kY">
